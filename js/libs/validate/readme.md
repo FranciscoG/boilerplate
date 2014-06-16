bd1d12a60329ed6a68b5e8f13d3e44d964794c49
FranciscoG <frankjg@gmail.com>
2014-06-16 17:01:54 -0400

    finished the validate js, let the constant refactoring begin

--- js/libs/validate/readme.md
+++ js/libs/validate/readme.md
@@ -8,13 +8,16 @@ A simple library to run form field validation to check if user has correctly fil
 1. It <b>does not protect against XSS</b>, that should be handled on the server
 2. It does not handle UI, you take care of that in the error/success options
 
+Test it out here: <a href="http://jsfiddle.net/87rdD/9/">http://jsfiddle.net/87rdD/9/</a>
+
 ####in your JS:
 
 ```javascript
 var validate = new Validator({
   classname: "js-validate",         // this is optional, it will default to "js-validate" if not set
-  error: function($elem){},         // run this function on error, handle your UI here.
-  success: function($elem){},       // run this function on success, handle your UI here
+  error: function($elem){},         // run this function on each error, handle your UI here.
+  success: function($elem){},       // run this function on each success, handle your UI here
+  complete: function(){},           // runs this function only once at the very end
   debug: true or false              // adds more detailed info to console.log 
 });
 ```
@@ -25,11 +28,12 @@ Adding the classname to an input, select, or textarea makes that field a require
 
 #####Text inputs:
 You must add a data attribute that specifies what type of validation to perform on this input
-```
+```html
 <input type="text" class="js-validate" name="first_name" data-validate-type="name" />
 ```
 
 here are the different text validation types
+```
   data-validate-type="name"  
   data-validate-type="phone"  
   data-validate-type="email"  
@@ -37,12 +41,13 @@ here are the different text validation types
   data-validate-type="zip4"   - allows the optional 4 digits  
   data-validate-type="num"    - for fields that should only have numbers  
   data-validate-type="state"  - in case you decide to use a text input instead of a select for US states
+```
 
 #####Textareas
 
 Adding the classname to a textarea only checks if it is empty or not. 
 
-```
+```html
 <textarea class="js-validate"></textarea>
 ```
 
@@ -52,7 +57,7 @@ This one is slightly different. If you require that a user must select an option
 data-validate-type="ignore"  
 actually that text can be anything, the code just looks for the existence of the data attribute
 
-```
+```html
 <select class="js-validate">
   <option value="none" selected="selected" data-validate-type="ignore">select</option>
   <option value="1">something<option>
@@ -60,18 +65,38 @@ actually that text can be anything, the code just looks for the existence of the
 </select>
 ```
 
-#####Checkbox
+#####Individual Checkbox
 just add the classname and it will make that checkbox required  
 tbd: multiple checkboxes with the same name
-```
+```html
 <input type="checkbox" class="js-validate" name="consent" /> I agree
 ```
 
-#####Radios
-When you added the classname it'll just make sure that one of the radios with the same name has been selected  
+#####A Group of Checkboxes
+You only need to add the classname to one of the checkbox elements in the same name attribute group  
+default will be that it makes sure that at least one checkbox is selected.  If you want to make more than one checkbox required then you'll neeed to add a data attibute like in this example:  
+
 ```
+data-validate-checkbox="max2"  - matches 1 or 2
+data-validate-checkbox="min2"  - matches 2 or more
+data-validate-checkbox="2"     - matches exactly 2
+```
+
+```html
+<p> where did you hear about this program , please choose at least 2</p>
+<input type="checkbox" class="js-validate" data-validate-checkbox="max2" name="survey" /> Online
+<input type="checkbox" name="survey" /> TV
+<input type="checkbox" name="survey" /> Radio
+<input type="checkbox" name="survey" /> Friend
+<input type="checkbox" name="survey" /> Other
+```
+
+#####Radios
+You only need to add the classname one radio in the same name attribute group. It will require that one of the radios is selected
+
+```html
 <input type="radio" class="js-validate" name="options" /> Red
-<input type="radio" class="js-validate" name="options" /> Blue
-<input type="radio" class="js-validate" name="options" /> Green
+<input type="radio" name="options" /> Blue
+<input type="radio" name="options" /> Green
 ```
 
--- js/libs/validate/validate.js
+++ js/libs/validate/validate.js
@@ -8,6 +8,7 @@
     options.classname = options.classname || "js-validate";
     options.success = options.success || null;
     options.error = options.error || null;
+    options.complete = options.complete || null;
     options.debug = options.debug || false;
 
     this.options = options;
@@ -60,22 +61,41 @@
       tag = $elem[0].tagName.toLowerCase();
     }
     
-
     switch (tag) {
+      case "select":
+        var $currentOption = $elem.find(":selected");
+        var dataAttr = $currentOption[0].getAttribute('data-validate-type');
+        isValid = ( dataAttr === null  ) ? true : false;
+        break;
       case "checkbox":
-        isValid = $elem.is(':checked');
+        var nameGroup = $elem.attr('name');
+        var $checked = $("input[name='"+nameGroup+"']:checked");
+        var dataAttr = $elem[0].getAttribute('data-validate-checkbox');
+        
+        if ( dataAttr !== null ) {
+          var total = parseFloat(dataAttr.replace(/[min|max]/g, ''));
+          var quantifier = dataAttr.replace(/[0-9]/g, '');
+
+          if (quantifier === "max") {
+            isValid = ($checked.length > 0 && $checked.length <= total) ? true : false;
+          } else if (quantifier === "min") {
+            isValid = ($checked.length >= total) ? true : false;
+          } else {
+            isValid = ($checked.length === total) ? true : false;
+          }
+
+        } else {
+          isValid = $elem.is(':checked');
+        }
+
         break;
       case "radio":
-        var currentRadioName = $elem.attr('name');
-        isValid = ( $("input[name='"+currentRadioName+"']").is(":checked") ) ? true : false;
+        var nameGroup = $elem.attr('name');
+        isValid = ( $("input[name='"+nameGroup+"']").is(":checked") ) ? true : false;
         break;
       case "textarea":
         isValid = ( !$.trim($elem.val()) ) ? false : true;
         break;
-      case "select":
-        var $currentOption = $elem.find(":selected");
-        var dataAttr = $currentOption.attr('data-validate-type');
-        isValid = ( typeof dataAttr === 'undefined' || dataAttr === false  ) ? false : true;
       default:
         console.warn('Validator: ' + tag + ' element is not supported');
         break;
@@ -128,8 +148,7 @@
           validate_result = /^(AA|AE|AP|AL|AK|AS|AZ|AR|CA|CO|CT|DE|DC|FM|FL|GA|GU|HI|ID|IL|IN|IA|KS|KY|LA|ME|MH|MD|MA|MI|MN|MS|MO|MT|NE|NV|NH|NJ|NM|NY|NC|ND|MP|OH|OK|OR|PW|PA|PR|RI|SC|SD|TN|TX|UT|VT|VI|VA|WA|WV|WI|WY)$/.test(val);
           break;
         default:
-          // default to the "name regex"
-          validate_result = /[a-zA-Z\d''-'\s]+/.test(_val);
+          console.warn('Validator: the data-validate-type you are using is not supported')
           break;
       };
 
@@ -143,26 +162,31 @@
   };
 
   Validator.prototype.handleResult = function($elem, result) {
+    $classname = $('.' + this.options.classname);
 
     if (!result && typeof this.options.error === 'function') {
       this.options.error($elem);
     } else if (result && typeof this.options.success === 'function') { 
-      this.options.success($elem);    
+      this.options.success($elem);
     } else {
       // do nothing for now
     }
 
+    if (this.index === $classname.length && typeof this.options.complete === "function" ) {
+      this.options.complete();
+    }
+
     // some debugging with nice formating in Chrome
-    if (this.options.debug && window.console) {
+    if (this.options.debug) {
       var currDebug =  {
-        tag: $elem[0].tagName,
+        tag: $elem[0].type || $elem[0].tagName,
         name: $elem.attr('name'),
         value: $elem.val(),
         pass: result
       };
 
       this.resultTable.push(currDebug);
-      if (this.index === $('.' + this.options.classname).length) {
+      if (this.index === $classname.length) {
         console.table(this.resultTable);
         console.log(this.errors + " errors found");
       }
@@ -173,20 +197,3 @@
   return window.Validator = Validator;
 
 })(window,document);
-
-$('#submit').click(function(e) {
-    e.preventDefault();
-    
-    var validate = new Validator({
-      classname: "js-validate",         
-      error: function($elem){
-          $elem.next('.error').show();
-      },         
-      success: function($elem){
-          $elem.next('.error').hide();
-      },       
-      debug: true
-    });
-});
-
-console.clear();
